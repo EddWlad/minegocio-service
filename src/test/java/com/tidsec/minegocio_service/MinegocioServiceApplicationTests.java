59f1e790ba744e5861c61289712b1e97adc55ccf
EddWlad <tidsec_inf@outlook.com>
2025-04-23 22:06:10 -0500

    Inicializacion de proyecto

---
+++ src/test/java/com/tidsec/minegocio_service/MinegocioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidsec.minegocio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinegocioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
